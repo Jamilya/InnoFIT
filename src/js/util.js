e484293bac080852251ba51ab3c013c1487ee2a0
Florian <fgrassinger@gmx.at>
2020-02-03 08:53:20 +0100

    Adding more style to other pages (matrix and deliveryplans)

--- src/js/util.js
+++ src/js/util.js
@@ -15,3 +15,13 @@ const getAppropriateDimensions = () => {
         return { width: defaultWidth, height: defaultHeight };
     }
 }
+
+const getPercentToPixelDimensions = (widthPercent = 100, heightPercent = 100) => {
+    const screenW = window.screen.width;
+    const screenH = window.screen.height;
+
+    const newWidth = screenW * (widthPercent / 100);
+    const newHeight = screenH * (heightPercent / 100);
+
+    return { width: newWidth, height: newHeight };
+}
